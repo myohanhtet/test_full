ec7acba4f0c23d5589a0b98dec161cab5e46726c
Ko Phyo <myohanhtet.ly@gmail.com>
2022-05-18 15:17:04 +0630

    added category

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Traits\FlashMessages;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class BaseController extends Controller
@@ -18,6 +19,69 @@ class BaseController extends Controller
     protected function setPageTitle($title, $subTitle)
     {
         view()->share(['pageTitle' => $title, 'subTitle' => $subTitle]);
+    }
+
+    /**
+     * @param int $errorCode
+     * @param null $message
+     * @return \Illuminate\Http\Response
+     */
+    protected function showErrorPage($errorCode = 404, $message = null)
+    {
+        $data['message'] = $message;
+        return response()->view('errors.'.$errorCode, $data, $errorCode);
+    }
+
+    /**
+     * @param bool $error
+     * @param int $responseCode
+     * @param array $message
+     * @param null $data
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function responseJson($error = true, $responseCode = 200, $message = [], $data = null)
+    {
+        return response()->json([
+            'error'         =>  $error,
+            'response_code' => $responseCode,
+            'message'       => $message,
+            'data'          =>  $data
+        ]);
+    }
+
+    /**
+     * @param $route
+     * @param $message
+     * @param string $type
+     * @param bool $error
+     * @param bool $withOldInputWhenError
+     * @return RedirectResponse
+     */
+    protected function responseRedirect($route, $message, string $type = 'info', bool $error = false, bool $withOldInputWhenError = false)
+    {
+        $this->setFlashMessage($message, $type);
+        $this->showFlashMessages();
+
+        if ($error && $withOldInputWhenError) {
+            return redirect()->back()->withInput();
+        }
+
+        return redirect()->route($route);
+    }
+
+    /**
+     * @param $message
+     * @param string $type
+     * @param bool $error
+     * @param bool $withOldInputWhenError
+     * @return RedirectResponse
+     */
+    protected function responseRedirectBack($message, string $type = 'info', bool $error = false, bool $withOldInputWhenError = false)
+    {
+        $this->setFlashMessage($message, $type);
+        $this->showFlashMessages();
+
+        return redirect()->back();
     }
 
 }
--- routes/admin.php
+++ routes/admin.php
@@ -4,8 +4,8 @@
 Route::group(['prefix'  =>  'admin'], function () {
 
     Route::get('login', [\App\Http\Controllers\Admin\LoginController::class,'showLoginForm'])->name('admin.login');
-    Route::post('login', 'Admin\LoginController@login')->name('admin.login.post');
-    Route::get('logout', 'Admin\LoginController@logout')->name('admin.logout');
+    Route::post('login', [\App\Http\Controllers\Admin\LoginController::class,'login'])->name('admin.login.post');
+    Route::get('logout', [\App\Http\Controllers\Admin\LoginController::class,'logout'])->name('admin.logout');
 
     Route::group(['middleware' => ['auth:admin']], function () {
 
